9aac63466cc2db09fcd0d34e2514f9d8644d6131
WenXin Xu <wenxinxu@WenXins-MacBook-Air.local>
2025-11-15 23:59:17 +0800

    add setup script

---
+++ setup_linux.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# 有任何失敗就退出
+set -e
+
+echo "🚀 正在開始設定新的 Linux 環境..."
+
+sudo apt update
+sudo apt install -y zsh\
+    tmux
+
+# setup oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# zsh-highlight
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# setup .vimrc
+cp .vimrc ~
+
+# setup tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp .tmux.conf ~
