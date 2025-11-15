f1af59188d9478ea1b8505c16737f0b3c302db44
WenXin Xu <wenxinxu@WenXins-MacBook-Air.local>
2025-11-16 00:31:45 +0800

    automate oh my zsh setup

--- setup_linux.sh
+++ setup_linux.sh
@@ -10,7 +10,7 @@ sudo apt install -y zsh\
     tmux
 
 # setup oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh-highlight
