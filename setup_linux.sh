b9347b16ce5c5c6a16811f0fc2285bc3c973bf4e
WenXin Xu <wenxinxu@WenXins-MacBook-Air.local>
2025-11-16 00:40:01 +0800

    add zshrc to ~

--- setup_linux.sh
+++ setup_linux.sh
@@ -15,6 +15,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh-highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp .zshrc ~
 
 # setup .vimrc
 cp .vimrc ~
