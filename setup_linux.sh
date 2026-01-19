65c4aae2380278408230f440666106f470d906ef
Xin401 <wenxinxu0401@gmail.com>
2026-01-19 15:49:16 +0800

    change structure

--- setup_linux.sh
+++ setup_linux.sh
@@ -9,18 +9,18 @@ sudo apt update
 sudo apt install -y zsh\
     tmux
 
-# setup oh-my-zsh
+# setup zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sudo chsh -s $(which zsh) $(whoami)
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh-highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cp .zshrc ~
+cp zsh/.zshrc ~
 
 # setup .vimrc
-cp .vimrc ~
+cp vim/.vimrc ~
 
 # setup tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp .tmux.conf ~
+cp tmux/.tmux.conf ~
